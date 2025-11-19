61609683996ad581e0974c48168b9aaa4c948b9d
Quang Phu <quangphu2424@gmail.com>
2025-11-19 22:33:57 +0700

    fix

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://boydohieu-backend.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://boydohieu-backend.onrender.com/api';
+   // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 
    export const getProducts = () => axios.get(`${API_URL}/products`);
    export const getProductById = (id) => axios.get(`${API_URL}/products/${id}`);
